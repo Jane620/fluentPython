1c1c01464747cb130edbbb6b98c18187b5a0b274
jwang <jianfeng.2.wang@nokia-sbell.com>
2019-11-22 16:12:59 +0800

    remove info

--- tests/ut/chapter1/__init__.py
+++ tests/ut/chapter1/__init__.py
@@ -1,10 +1,1 @@
 # -*- coding:utf-8 -*-
-'''
-@author: jwang
-@license: (C) Copyright 2018-2019, Nokia Sbell Tech Corporation Limited.
-@contact: jianfeng.2.wang@nokia-sbell.com
-@software: Pycharm
-@file: __init__.py
-@time: 2019/11/4 17:21
-@desc:
-'''
--- tests/ut/chapter2/test_cartesian.py
+++ tests/ut/chapter2/test_cartesian.py
@@ -1,13 +1,4 @@
 # -*- coding:utf-8 -*-
-'''
-@author: jwang
-@license: (C) Copyright 2018-2019, Nokia Sbell Tech Corporation Limited.
-@contact: jianfeng.2.wang@nokia-sbell.com
-@software: Pycharm
-@file: test_cartesian.py
-@time: 2019/11/19 17:08
-@desc:
-'''
 import pytest
 from src.chapter2.cartesian_list import cartesian_out
 
